74a06bd959244e0a2872ad8eedf05f5d5388f826
Jeevan <jeevan.panjwani@ctx.edu>
2018-11-16 17:56:27 +0000

    Completed Jake and Jeevan

--- pa6-2.s
+++ pa6-2.s
@@ -1,4 +1,8 @@
-		@---------------------
+		@---------------------\
+		@Jacob Mathieu & Jeevan Panjwani
+		@PA6-2.s
+		@
+		@
 		@	Programming Assignment 6, Problem 2
 		@	Your required header information goes here
 		@--------------------
@@ -13,7 +17,7 @@
 		@	5
 	.extern printf
         .global main
-        .func main 		
+        .func main
 main:		@	DO NOT TOUCH!
 		PUSH	{LR}
 		MOV	R0, #5
@@ -47,14 +51,39 @@ main:		@	DO NOT TOUCH!
 		LDR	R0, =out
 		BL 	printf
 		POP	{PC}
-		
+
 		@---------------------
 		@	div function.  Your code goes here!
 		@---------------------
 div:		PUSH	{LR}	@ Leave this here
+            PUSH {R2-R14}  @push R2-R14 into a stack
+            MOV R2, R1     @ Moves Dividen into R2
+            MOV R1, R0     @ moves divisor into R1
+
+            MOV R3, R2
+            CMP R3, R1, LSR #1 @ LSR halves the number in R1 and puts that value into R3
+
+Div1:
+            MOVLS R3, R3, LSL #1 @Doubles the value in R3 putting the value into R3 if lower than or equal to R3
+            CMP R3, R1, LSR #1 @ Compares the halved value of R1 with R3
+            BLS Div1  @ branches div1 function if less than
+            MOV R4, #0 @moves 0 into R4
+
+Div2:       CMP R1, R3  @Compares R3value to R1 value
+            SUBCS R1, R1, R3 @If result is bigger than 32 bits subtract R1 from R3 putting the value into R1
+            ADC R4, R4, R4
+
+            MOV R3, R3, LSR #1  @half the value in R3 and put into register R3
+            CMP R3, R2 @compares the value in R2 with R3
+            BHS Div2 @Exits the loop of div 2 ifHs is active
+
+            MOV R0, R4 @movs value from R4 into R0
+            POP {R2-R14}
+            POP {PC}
+
 
 		@ Your code here
-	
+
 		POP	{PC} 	@ return statement
 .data
 out:	.asciz "%d\n"	@ String for printf
